418b9b24f4f9d7d381fea841e1328a35e1ae1d39
woai3c <411020382@qq.com>
2018-12-09 15:29:24 +0800

    add vueform

--- src/App.vue
+++ src/App.vue
@@ -1,28 +1,37 @@
 <template>
-  <div id="app">
-    <img src="./assets/logo.png">
-    <HelloWorld/>
-  </div>
+    <div id="app">
+        <VueForm :options="options"/>
+    </div>
 </template>
 
 <script>
-import HelloWorld from './components/HelloWorld'
+import VueForm from './components/VueForm'
 
 export default {
-  name: 'App',
-  components: {
-    HelloWorld
-  }
+    name: 'App',
+    data() {
+        return {
+            options: [
+                {
+                    type: 'input',
+                    attrs: {
+                        type: 'text',
+                        placeholder: 'Enter name...',
+                    },
+                    value: 'test',
+                    rules: {
+                        
+                    }
+                }
+            ]
+        }
+    },
+    components: {
+        VueForm
+    }
 }
 </script>
 
 <style>
-#app {
-  font-family: 'Avenir', Helvetica, Arial, sans-serif;
-  -webkit-font-smoothing: antialiased;
-  -moz-osx-font-smoothing: grayscale;
-  text-align: center;
-  color: #2c3e50;
-  margin-top: 60px;
-}
+
 </style>
---
+++ src/components/VueForm.vue
@@ -0,0 +1,27 @@
+<script>
+import Schema from 'async-validator'
+
+export default {
+    name: 'HelloWorld',
+    data () {
+        return {
+            formData: {}
+        }
+    },
+    render(h) {
+        const self = this
+        const options = this.options
+
+        const components = options.map
+        return h('form', )
+    }
+}
+
+function generateInputComponent(h, formData,) {
+
+}
+</script>
+
+<style scoped>
+
+</style>
